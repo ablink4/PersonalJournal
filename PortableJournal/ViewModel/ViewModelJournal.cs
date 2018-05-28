76083c8df304da0542ffc54e5d4e714addeb116d
ablink4 <aaron.blinka@gmail.com>
2018-05-28 17:20:38 -0500

    Just cleaning up some unused code in the Journal View and ViewModel.

--- PortableJournal/ViewModel/ViewModelJournal.cs
+++ PortableJournal/ViewModel/ViewModelJournal.cs
@@ -24,36 +24,5 @@ namespace PortableJournal.ViewModel
                 return JournalDatabase.GetOpenJournal().Name;
             }
         }
-
-        public string JournalContent
-        {
-            get
-            {
-                // everything about this is wrong
-                Journal activeJournal = JournalDatabase.GetOpenJournal();
-
-                if (activeJournal.Entries == null)
-                {
-                    activeJournal.Entries = new System.Collections.Generic.List<JournalEntry>();
-                }
-                if(activeJournal.Entries.Count == 0)
-                { 
-                    activeJournal.Entries.Add(new JournalEntry("New Entry"));
-                    activeJournal.Entries[0].FullText = "This is some placeholder text.";
-                    JournalDatabase.UpdateJournal(activeJournal); // update with the new entries
-                }                
-
-                return activeJournal.Entries[0].FullText;
-            }
-            set
-            {
-                // if the added character is whitespace, it won't be saved... (FIXME)
-                Journal activeJournal = JournalDatabase.GetOpenJournal();
-                activeJournal.Entries[0].FullText = value;
-                JournalDatabase.UpdateJournal(activeJournal);
-                RaisePropertyChanged("JournalContent");
-            }
-        }
-
     }
 }
