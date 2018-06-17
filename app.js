25e1bc43b086c99156412daea24416d8bf9697fb
ablink4 <aaron.blinka@gmail.com>
2018-06-17 17:44:50 -0500

    Removed sqldb file.  Separated journals and entries sections on the page.  Added buttons for navigating through journal entries.  Added a function, printError, to clean up the code.

--- app.js
+++ app.js
@@ -3,41 +3,25 @@ const sqlite3 = require('sqlite3').verbose();
 const saveButton = document.getElementById('save-button');
 const journalContent = document.getElementById('journal-content');
 
-var db = new sqlite3.Database('./sqldb', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-});
+var db = new sqlite3.Database('./sqldb', (err) => printError(err));
 
 db.serialize(() => {
     const createQuery = `CREATE TABLE IF NOT EXISTS journal (text);`;
-    db.run(createQuery, (err) => {
-        if(err) {
-            console.error(err.message);
-        }
-    });
+    db.run(createQuery, (err) => printError(err));
 
     db.all(`SELECT * FROM journal;`, (err, rows) => {
-        if(err) {
-            console.error(err.message);
-        }
+        printError(err);
 
         if(rows.length === 0) {
             var insertQuery = `INSERT INTO journal (text) VALUES(?)`;
-            db.run(insertQuery, [''], (err) => {
-                if(err) {
-                    console.error(err.message);
-                }
-            });
+            db.run(insertQuery, [''], (err) => printError(err));
         }
         else {
             const selectQuery = `SELECT * from journal;`;
 
             // this only works because there's 1 row in the table
             db.each(selectQuery, (err, row) => {
-                if(err) {
-                    console.error(err.message);
-                }
+                printError(err);
 
                 if(row) {
                     journalContent.value = row.text;
@@ -52,9 +36,11 @@ saveButton.addEventListener('click', () => {
     if(!text) return;
 
     var query = `UPDATE journal SET text = ?`;
-    db.run(query, [text], (err) => {
-        if(err) {
-            console.error(err.message);
-        }
-    });
+    db.run(query, [text], (err) => printError(err));
 });
+
+function printError(err) {
+    if(err) {
+        console.error(err.message);
+    }
+}
